50f5b74377764352ffa33d37da26663c533781d1
Sung Ye In <syi9397@naver.com>
2021-07-05 19:43:06 +0900

    update palatte move point

--- 색상추출 돋보기/index.js
+++ 색상추출 돋보기/index.js
@@ -12,10 +12,24 @@ window.onload = () => {
 class App {
     constructor() {
         this.canvas = document.createElement("canvas");
-        document.getElementById("paletteWrapper").appendChild(this.canvas);
+        this.wrapper = document.createElement("div");
+        this.wrapper.appendChild(this.canvas);
+        this.wrapper.style.position = "relative";
+        document.getElementById("paletteWrapper").appendChild(this.wrapper);
         this.canvas.setAttribute("id", "palette");
         this.ctx = this.canvas.getContext("2d");
         this.color = "#ff0000";
+
+        this.radius = 12;
+
+        this.isClick = false;
+
+        this.hue = 0;
+
+        this.rgb = [255, 0, 0];
+
+        this.x = 300;
+        this.y = 0;
 
         this.init();
     }
@@ -26,7 +40,30 @@ class App {
         this.canvas.width = this.stageWidth;
         this.canvas.height = this.stageHeight;
         this.drawPalette();
-        this.gradientSlider = new GradientSlider(this.changeColor.bind(this));
+        this.gradientSlider = new GradientSlider({
+            changeColor: this.changeColor.bind(this),
+            changeHue: this.changeHue.bind(this),
+        });
+
+        this.wrapper.addEventListener("mousedown", () => {
+            this.isClick = true;
+        });
+        window.addEventListener("mousemove", this.move.bind(this));
+        window.addEventListener("mouseup", () => {
+            this.isClick = false;
+        });
+        this.createMovePoint();
+    }
+
+    move(e) {
+        if (!this.isClick) return;
+        this.x = e.clientX - getDisLeft(this.wrapper);
+        this.y = e.clientY - getDisTop(this.wrapper);
+        if (this.x < 0) this.x = 0;
+        if (this.x > this.stageWidth) this.x = this.stageWidth;
+        if (this.y < 0) this.y = 0;
+        if (this.y > this.stageHeight) this.y = this.stageHeight;
+        this.drawMovePoint();
     }
 
     drawPalette() {
@@ -58,18 +95,60 @@ class App {
         this.ctx.fillRect(0, 0, this.stageWidth, this.stageHeight);
     }
 
+    createMovePoint() {
+        this.movePoint = document.createElement("div");
+        this.circle = document.createElement("div");
+
+        this.circle.appendChild(this.movePoint);
+
+        this.wrapper.appendChild(this.circle);
+
+        this.circle.style.position = "absolute";
+        this.circle.style.display = "flex";
+        this.circle.style.alignItems = "center";
+        this.circle.style.justifyContent = "center";
+        this.circle.style.width = `${this.radius * 2}px`;
+        this.circle.style.height = `${this.radius * 2}px`;
+        this.circle.style.borderRadius = "50%";
+        this.circle.style.right = 0;
+        this.circle.style.top = 0;
+        this.circle.style.cursor = "pointer";
+        this.circle.style.background = "white";
+        this.circle.style.transform = `translate(${this.radius}px, ${-this
+            .radius}px)`;
+        this.movePoint.style.background = "red";
+        this.movePoint.style.width = `${this.radius}px`;
+        this.movePoint.style.height = `${this.radius}px`;
+        this.movePoint.style.borderRadius = "50%";
+    }
+
+    drawMovePoint() {
+        this.rgb = locationToRgb(this.hue / 360, this.x, this.y);
+        this.circle.style.transform = `translate(${
+            this.x - 300 + this.radius
+        }px, ${this.y - this.radius}px)`;
+        this.movePoint.style.background =
+            `rgb(${this.rgb[0]}, ${this.rgb[1]}, ${this.rgb[2]})` || "white";
+    }
+
     changeColor(color) {
         this.color = color;
         this.drawPalette();
     }
+
+    changeHue(hue) {
+        this.hue = hue;
+        this.drawMovePoint();
+    }
 }
 
 class GradientSlider {
-    constructor(props) {
+    constructor({ changeColor, changeHue }) {
         this.canvas = document.createElement("canvas");
         this.canvas.setAttribute("id", "gradient-slider");
         this.wrapper = document.createElement("div");
         this.wrapper.style.position = "relative";
+        this.wrapper.style.marginLeft = "20px";
         this.wrapper.appendChild(this.canvas);
         document.getElementById("paletteWrapper").appendChild(this.wrapper);
         this.ctx = this.canvas.getContext("2d");
@@ -81,7 +160,8 @@ class GradientSlider {
 
         this.clicked = false;
 
-        this.changeColor = props;
+        this.changeColor = changeColor;
+        this.changeHue = changeHue;
 
         this.init();
     }
@@ -110,15 +190,15 @@ class GradientSlider {
         this.y = e.clientY - getDisTop(this.wrapper) - this.radius;
         if (this.y + this.radius < 0 || this.y + this.radius > this.stageHeight)
             return;
+        const huePercent = (this.y + this.radius) / this.stageHeight;
         // const [r, g, b] = this.ctx.getImageData(
         //     (this.stageWidth - this.width) / 2,
         //     this.y + this.radius,
         //     1,
         //     1
         // ).data;
-        const [r, g, b] = spectrumPercentToRgb(
-            (this.y + this.radius) / this.stageHeight
-        );
+        const [r, g, b] = spectrumPercentToRgb(huePercent);
+        this.changeHue(360 - huePercent * 360);
         this.changeColor(rgbToHex(r, g, b));
         this.drawMovePoint(rgbToHex(r, g, b));
     }
@@ -216,6 +296,10 @@ function getDisTop(target) {
     return target.getBoundingClientRect().top;
 }
 
+function getDisLeft(target) {
+    return target.getBoundingClientRect().left;
+}
+
 function hslToRgb(h, s, l) {
     let r, g, b;
 
@@ -241,6 +325,45 @@ function hslToRgb(h, s, l) {
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
+function rgbToHsl(r, g, b) {
+    (r /= 255), (g /= 255), (b /= 255);
+    var max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    var h,
+        s,
+        l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+
 function spectrumPercentToRgb(percent) {
     return hslToRgb(1 - percent, 1, 0.5);
+}
+
+function locationToRgb(h, x, y) {
+    const hsv_value = 1 - y / 300;
+    const hsv_saturation = x / 300;
+    const lightness = (hsv_value / 2) * (2 - hsv_saturation);
+    const saturation =
+        (hsv_value * hsv_saturation) / (1 - Math.abs(2 * lightness - 1));
+    return hslToRgb(h, saturation, lightness);
 }
